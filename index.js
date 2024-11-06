f1336596af8992740efdffd27d5951faf7db6745
Dexedus <karlfleming64@gmail.com>
2024-11-06 11:47:58 +0000

    add update functionality and add ability to filter by author of review

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+//Databse
 const db = new pg.Client({
     user: process.env.un,
     host: "localhost",
@@ -17,52 +18,58 @@ const db = new pg.Client({
   });
   db.connect();
 
+//Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let posts = []
+let posts = [];
 
+//Get All Posts
 app.get("/", async (req, res) =>{
 
-    let data = await db.query("SELECT * FROM posts")
+    let data = await db.query("SELECT * FROM posts");
     posts = data.rows
-    console.log(posts)
+    console.log(posts);
     res.render("index.ejs",{
         posts: posts,
-    })
-})
+    });
+});
 
+//Get all posts in order of ascending ID
 app.get("/asc", async (req, res) =>{
 
-    let data = await db.query("SELECT * FROM posts ORDER BY id ASC")
+    let data = await db.query("SELECT * FROM posts ORDER BY id ASC");
     posts = data.rows
     console.log(posts)
     res.render("index.ejs",{
         posts: posts,
-    })
-})
+    });
+});
 
+// Get all posts in order of descending ID
 app.get("/desc", async (req, res) =>{
 
-    let data = await db.query("SELECT * FROM posts ORDER BY id DESC")
+    let data = await db.query("SELECT * FROM posts ORDER BY id DESC");
     posts = data.rows
     console.log(posts)
     res.render("index.ejs",{
         posts: posts,
-    })
-})
+    });
+});
 
+// Load the new entry page
 app.get("/add", async (req, res) =>{
     res.render("new.ejs")
-})
+});
 
+// Add entry to database
 app.post("/submit", async (req, res) =>{
     let author = req.body.author;
     let book = req.body.book;
     let review = req.body.review;
     let rating = req.body.rating;
 
-    const result = await axios.get(`https://openlibrary.org/search.json?q=${book}&limit=1`)
+    const result = await axios.get(`https://openlibrary.org/search.json?q='%'||${book}||'%'&limit=1`);
     let coverID = result.data.docs[0].cover_edition_key;
     
     const fullDate = new Date()
@@ -76,19 +83,48 @@ app.post("/submit", async (req, res) =>{
 
 
     res.redirect("/");
-})
+});
 
-app.get("/update:id", (req, res) =>{
+//Load the update page
+app.get("/update:id", async (req, res) =>{
     let id = req.params.id;
-    
+    let data = await db.query("SELECT * FROM posts WHERE id = ($1)",[id]);
+    posts = data.rows[0];
+    res.render("new.ejs", {
+        posts: posts,
+    });
+
+    console.log(posts)
+});
+
+//Update post
+app.post("/edit:id", async (req, res) =>{
+    let id = req.params.id;
+    let author = req.body.author;
+    let book = req.body.book;
+    let review = req.body.review;
+    let rating = req.body.rating;
+    await db.query("UPDATE posts SET author = ($1), descr = ($2), rating = ($3), book_auth = ($4) WHERE id = ($5)", [author, review, rating, book, id]);
+    res.redirect("/");
 })
 
+//Filter by author of Review
+app.get("/:auth", async (req, res) =>{
+    let author = req.params.auth
+    let data = await db.query("SELECT * FROM posts WHERE author = ($1)", [author])
+    posts = data.rows; 
+    res.render("index.ejs", {
+        posts: posts,
+    })
+})
+
+//Delete post
 app.get("/delete:id", async (req, res) =>{
     let id = req.params.id
     await db.query("DELETE FROM posts WHERE id = ($1)", [id])
 
     res.redirect("/")
-})
+});
 
 
 
